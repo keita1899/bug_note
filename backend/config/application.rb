dee650f6ff664a7d2cfb19bcf4bb9c7e1483183d
keita1899 <saboten18k@gmail.com>
2025-02-23 21:05:46 +0900

    chore: 日本語化の設定

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -52,5 +52,8 @@ module Backend
                        helper_specs: false,
                        request_specs: true
     end
+
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
   end
 end
