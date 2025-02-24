9d2fcbb8c5c6d12bd24cefb127e571677f6b2e4c
keita1899 <saboten18k@gmail.com>
2025-02-24 13:42:42 +0900

    style: Tailwindの基本のスタイルの設定

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: {
+          600: '#7C3AED',
+          700: '#6A28B3',
+        },
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['light'],
+  },
 }
