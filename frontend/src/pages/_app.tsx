c13b62ba0d1012ff82259885a2394eb6b276ee56
keita1899 <saboten18k@gmail.com>
2025-02-24 10:38:48 +0900

    npm run format

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,9 +1,6 @@
 import '@/styles/globals.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
 
