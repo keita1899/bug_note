2583d43adf12237963d8cca445f35e3dc67cc894
keita1899 <saboten18k@gmail.com>
2025-03-01 21:37:33 +0900

    feat: ログインユーザーを取得するAPIの処理を実装

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       get "hello", to: "hello#index"
       mount_devise_token_auth_for "User", at: "auth"
+      namespace :current do
+        resource :user, only: [:show]
+      end
       namespace :user do
         resource :confirmations, only: [:update]
       end
