06af404f31dd8de0eb0baba396c62f68462c8eab
keita1899 <saboten18k@gmail.com>
2025-03-09 22:17:39 +0900

    test: バグ一覧のリクエストスペック作成

--- backend/spec/requests/api/v1/bugs_spec.rb
+++ backend/spec/requests/api/v1/bugs_spec.rb
@@ -1,9 +1,69 @@
 require "rails_helper"
 
 RSpec.describe "Api::V1::Bug", type: :request do
+  let!(:user) { create(:user) }
+  let(:headers) { user.create_new_auth_token }
+
+  describe "GET /api/v1/bugs" do
+    let!(:bugs) { create_list(:bug, 30, user: user, status: "published") }
+    let!(:unpublished_bug) { create(:bug, user: user, status: "draft") }
+
+    context "ログインしている場合" do
+      before do
+        get "/api/v1/bugs", headers: headers
+      end
+
+      it "ステータスコード200が返る" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "公開されているバグのリストが返る" do
+        json = JSON.parse(response.body)
+        expect(json["bugs"].size).to eq(10)
+        expect(json["bugs"].pluck("id").sort).to eq(bugs.last(10).pluck(:id).sort)
+      end
+
+      it "未公開のバグは含まれない" do
+        json = JSON.parse(response.body)
+        expect(json["bugs"].pluck("id")).not_to include(unpublished_bug.id.to_s)
+      end
+
+      it "レスポンスにページネーションのメタデータが含まれる" do
+        json = JSON.parse(response.body)
+        expect(json["meta"]["total_pages"]).to eq(3)
+        expect(json["meta"]["current_page"]).to eq(1)
+      end
+    end
+
+    context "ログインしていない場合" do
+      before do
+        get "/api/v1/bugs"
+      end
+
+      it "ステータスコード200が返る" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "公開されているバグのリストが返る" do
+        json = JSON.parse(response.body)
+        expect(json["bugs"].size).to eq(10)
+        expect(json["bugs"].pluck("id").sort).to eq(bugs.last(10).pluck(:id).sort)
+      end
+
+      it "未公開のバグは含まれない" do
+        json = JSON.parse(response.body)
+        expect(json["bugs"].pluck("id")).not_to include(unpublished_bug.id.to_s)
+      end
+
+      it "レスポンスにページネーションのメタデータが含まれる" do
+        json = JSON.parse(response.body)
+        expect(json["meta"]["total_pages"]).to eq(3)
+        expect(json["meta"]["current_page"]).to eq(1)
+      end
+    end
+  end
+
   describe "POST /api/v1/bugs" do
-    let!(:user) { create(:user) }
-    let(:headers) { user.create_new_auth_token }
     let(:valid_params) do
       {
         title: "Example Bug Title",
