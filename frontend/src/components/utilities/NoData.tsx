081b4718696f306f24091622e1fb85820893ebba
keita1899 <saboten18k@gmail.com>
2025-03-09 23:12:23 +0900

    npm run format

--- frontend/src/components/utilities/NoData.tsx
+++ frontend/src/components/utilities/NoData.tsx
@@ -1,5 +1,5 @@
 export const NoData = () => (
-  <div className="p-4 text-center pt-32">
+  <div className="p-4 pt-32 text-center">
     <p className="text-lg font-semibold text-gray-600">データがありません</p>
   </div>
 )
