48e681e76e3e57616789ae9ccaf6196a0396bb52
keita1899 <saboten18k@gmail.com>
2025-03-10 18:04:14 +0900

    refactor: headersの指定を修正

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,9 +1,10 @@
 import axios, { AxiosResponse, AxiosError } from 'axios'
 import camelcaseKeys from 'camelcase-keys'
+import { getAuthHeaders } from './headers'
 
-export const fetcher = (url: string) =>
+export const fetcher = (url: string, headers = getAuthHeaders()) =>
   axios
-    .get(url)
+    .get(url, { headers })
     .then((res: AxiosResponse) => camelcaseKeys(res.data, { deep: true }))
     .catch((err: AxiosError) => {
       console.log(err.message)
