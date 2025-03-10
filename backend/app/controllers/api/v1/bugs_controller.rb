b0e592c8e72d502dc91e8ba2331c995dc61868e0
keita1899 <saboten18k@gmail.com>
2025-03-10 18:02:45 +0900

    feat: バグ詳細取得API作成

--- backend/app/controllers/api/v1/bugs_controller.rb
+++ backend/app/controllers/api/v1/bugs_controller.rb
@@ -8,6 +8,26 @@ class Api::V1::BugsController < Api::V1::BaseController
     render json: bugs, each_serializer: BugSerializer, status: :ok, meta: pagination(bugs), adapter: :json
   end
 
+  def show
+    bug = if user_signed_in?
+            Bug.includes(:user, :environments, :attempts, :references).
+              where(id: params[:id]).
+              where("user_id = ? OR (user_id != ? AND status = ?)", current_user.id, current_user.id, "published").
+              first
+          else
+            Bug.includes(:user, :environments, :attempts, :references).
+              where(id: params[:id]).
+              where(status: "published").
+              first
+          end
+
+    if bug
+      render json: bug, serializer: BugSerializer
+    else
+      render json: { error: "Bug not found" }, status: :not_found
+    end
+  end
+
   def create
     form = BugForm.new(bug_params, current_user)
 
