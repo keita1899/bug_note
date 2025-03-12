5013cb36e5c0f0805a4f95407c82663091eb8aff
keita1899 <saboten18k@gmail.com>
2025-03-12 14:03:52 +0900

    test: バグ更新のリクエストスペック作成

--- backend/spec/requests/api/v1/bugs_spec.rb
+++ backend/spec/requests/api/v1/bugs_spec.rb
@@ -8,49 +8,10 @@ RSpec.describe "Api::V1::Bug", type: :request do
   let!(:user) { create(:user) }
   let(:other_user) { create(:user) }
   let(:headers) { user.create_new_auth_token }
-  let(:valid_params) do
-    {
-      title: "Example Bug Title",
-      error_message: "Example error message",
-      content: "Example bug content",
-      expected_behavior: "Expected behavior content",
-      solution: "Solution to the bug",
-      cause: "Root cause of the bug",
-      etc: "Other info",
-      is_solved: false,
-      status: "draft",
-      environments: [
-        { category: "プログラミング言語", name: "Ruby", version: "2.7.0" },
-      ],
-      attempts: [
-        { content: "Tried restarting the server" },
-      ],
-      references: [
-        { url: "http://example.com" },
-      ],
-    }
-  end
-
-  let(:invalid_params) do
-    {
-      title: "",
-      error_message: "",
-      content: "",
-      expected_behavior: "",
-      solution: "",
-      cause: "",
-      etc: "",
-      is_solved: false,
-      status: "draft",
-      environments: [],
-      attempts: [],
-      references: [],
-    }
-  end
 
   describe "GET /api/v1/bugs" do
     let!(:bugs) { create_list(:bug, 25, user: user, status: "published") }
-    let!(:draft_bug) { create(:bug, user: user) }
+    let(:draft_bug) { create(:bug, user: user) }
 
     context "ログインしている場合" do
       before do
@@ -69,7 +30,7 @@ RSpec.describe "Api::V1::Bug", type: :request do
           expect(response_json["bugs"].pluck("id")).to eq(bugs.sort_by(&:created_at).reverse.first(10).pluck(:id))
         end
 
-        it "未公開のバグは含まれない" do
+        it "下書きのバグは含まれない" do
           expect(response_json["bugs"].pluck("id")).not_to include(draft_bug.id.to_s)
         end
 
@@ -91,7 +52,7 @@ RSpec.describe "Api::V1::Bug", type: :request do
           expect(response_json["bugs"].pluck("id")).to eq(bugs.sort_by(&:created_at).reverse[10..19].pluck(:id))
         end
 
-        it "未公開のバグは含まれない" do
+        it "下書きのバグは含まれない" do
           expect(response_json["bugs"].pluck("id")).not_to include(draft_bug.id.to_s)
         end
 
@@ -113,7 +74,7 @@ RSpec.describe "Api::V1::Bug", type: :request do
           expect(response_json["bugs"].pluck("id")).to eq(bugs.sort_by(&:created_at).reverse[20..24].pluck(:id))
         end
 
-        it "未公開のバグは含まれない" do
+        it "下書きのバグは含まれない" do
           expect(response_json["bugs"].pluck("id")).not_to include(draft_bug.id.to_s)
         end
 
@@ -138,7 +99,7 @@ RSpec.describe "Api::V1::Bug", type: :request do
         expect(response_json["bugs"].pluck("id")).to eq(bugs.sort_by(&:created_at).reverse.first(10).pluck(:id))
       end
 
-      it "未公開のバグは含まれない" do
+      it "下書きのバグは含まれない" do
         expect(response_json["bugs"].pluck("id")).not_to include(draft_bug.id.to_s)
       end
 
@@ -150,72 +111,72 @@ RSpec.describe "Api::V1::Bug", type: :request do
   end
 
   describe "GET /api/v1/bugs/:id" do
-    let(:published_bug) { create(:bug, :published, user: other_user) }
-    let(:draft_bug) { create(:bug, user: other_user) }
-    let(:own_draft_bug) { create(:bug, user: user) }
-    let(:own_solved_draft_bug) { create(:bug, :solved_draft, user: user) }
-    let(:own_published_bug) { create(:bug, :published, user: user) }
+    let(:other_published_bug) { create(:bug, :published, user: other_user) }
+    let(:other_draft_bug) { create(:bug, user: other_user) }
+    let(:draft_bug) { create(:bug, user: user) }
+    let(:solved_draft_bug) { create(:bug, :solved_draft, user: user) }
+    let(:published_bug) { create(:bug, :published, user: user) }
 
     context "ログインしている場合" do
       it "自分の下書きの未解決バグの詳細データが返る" do
-        get "/api/v1/bugs/#{own_draft_bug.id}", headers: headers
+        get "/api/v1/bugs/#{draft_bug.id}", headers: headers
         expect(response).to have_http_status(:ok)
-        expect(response_json["id"]).to eq(own_draft_bug.id)
+        expect(response_json["id"]).to eq(draft_bug.id)
         expect(response_json["is_solved"]).to be(false)
         expect(response_json["status"]).to eq("draft")
       end
 
       it "自分の下書きの解決済バグの詳細データが返る" do
-        get "/api/v1/bugs/#{own_solved_draft_bug.id}", headers: headers
+        get "/api/v1/bugs/#{solved_draft_bug.id}", headers: headers
         expect(response).to have_http_status(:ok)
-        expect(response_json["id"]).to eq(own_solved_draft_bug.id)
+        expect(response_json["id"]).to eq(solved_draft_bug.id)
         expect(response_json["is_solved"]).to be(true)
         expect(response_json["status"]).to eq("draft")
       end
 
       it "自分の公開中の解決済バグの詳細データが返る" do
-        get "/api/v1/bugs/#{own_published_bug.id}", headers: headers
+        get "/api/v1/bugs/#{published_bug.id}", headers: headers
         expect(response).to have_http_status(:ok)
-        expect(response_json["id"]).to eq(own_published_bug.id)
+        expect(response_json["id"]).to eq(published_bug.id)
         expect(response_json["is_solved"]).to be(true)
         expect(response_json["status"]).to eq("published")
       end
 
       it "他人の下書きのバグの詳細データが返されない" do
-        get "/api/v1/bugs/#{draft_bug.id}", headers: headers
+        get "/api/v1/bugs/#{other_draft_bug.id}", headers: headers
         expect(response).to have_http_status(:not_found)
       end
 
       it "他人の公開バグの詳細データが返る" do
-        get "/api/v1/bugs/#{published_bug.id}", headers: headers
+        get "/api/v1/bugs/#{other_published_bug.id}", headers: headers
         expect(response).to have_http_status(:ok)
-        expect(response_json["id"]).to eq(published_bug.id)
+        expect(response_json["id"]).to eq(other_published_bug.id)
         expect(response_json["status"]).to eq("published")
       end
     end
 
     context "ログインしていない場合" do
       it "自分の下書きのバグの詳細データが返されない" do
-        get "/api/v1/bugs/#{own_draft_bug.id}"
-        expect(response).to have_http_status(:not_found)
-      end
-
-      it "自分の公開中の解決済バグの詳細データが返る" do
-        get "/api/v1/bugs/#{own_published_bug.id}"
-        expect(response).to have_http_status(:ok)
-        expect(response_json["id"]).to eq(own_published_bug.id)
-        expect(response_json["status"]).to eq("published")
-      end
-
-      it "他人の下書きのバグの詳細データが返されない" do
         get "/api/v1/bugs/#{draft_bug.id}"
         expect(response).to have_http_status(:not_found)
       end
 
-      it "他人の公開バグの詳細データが返る" do
+      it "自分の公開中の解決済バグの詳細データが返る" do
         get "/api/v1/bugs/#{published_bug.id}"
         expect(response).to have_http_status(:ok)
         expect(response_json["id"]).to eq(published_bug.id)
+        expect(response_json["status"]).to eq("published")
+      end
+
+      it "他人の下書きのバグの詳細データが返されない" do
+        get "/api/v1/bugs/#{other_draft_bug.id}"
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it "他人の公開バグの詳細データが返る" do
+        get "/api/v1/bugs/#{other_published_bug.id}"
+        expect(response).to have_http_status(:ok)
+        expect(response_json["id"]).to eq(other_published_bug.id)
         expect(response_json["status"]).to eq("published")
       end
     end
@@ -227,6 +188,46 @@ RSpec.describe "Api::V1::Bug", type: :request do
   end
 
   describe "POST /api/v1/bugs" do
+    let(:valid_params) do
+      {
+        title: "Example Bug Title",
+        error_message: "Example error message",
+        content: "Example bug content",
+        expected_behavior: "Expected behavior content",
+        solution: "Solution to the bug",
+        cause: "Root cause of the bug",
+        etc: "Other info",
+        is_solved: false,
+        status: "draft",
+        environments: [
+          { category: "プログラミング言語", name: "Ruby", version: "2.7.0" },
+        ],
+        attempts: [
+          { content: "Tried restarting the server" },
+        ],
+        references: [
+          { url: "http://example.com" },
+        ],
+      }
+    end
+
+    let(:invalid_params) do
+      {
+        title: "",
+        error_message: "",
+        content: "",
+        expected_behavior: "",
+        solution: "",
+        cause: "",
+        etc: "",
+        is_solved: false,
+        status: "draft",
+        environments: [],
+        attempts: [],
+        references: [],
+      }
+    end
+
     context "ログインしている場合" do
       it "正しい値が入力された場合、バグの登録が成功する" do
         expect {
@@ -250,6 +251,93 @@ RSpec.describe "Api::V1::Bug", type: :request do
     context "ログインしていない場合" do
       it "401 エラーが返る" do
         post "/api/v1/bugs", params: valid_params, as: :response_json
+
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
+
+  describe "UPDATE /api/v1/bugs/:id" do
+    let(:bug) { create(:bug, user: user) }
+    let(:other_bug) { create(:bug, user: other_user) }
+
+    let(:valid_params) do
+      {
+        title: "Example Bug Title",
+        error_message: "Example error message",
+        content: "Example bug content",
+        expected_behavior: "Expected behavior content",
+        solution: "Solution to the bug",
+        cause: "Root cause of the bug",
+        etc: "Other info",
+        is_solved: false,
+        status: "draft",
+        environments: [
+          { category: "プログラミング言語", name: "Ruby", version: "2.7.0" },
+        ],
+        attempts: [
+          { content: "Tried restarting the server" },
+        ],
+        references: [
+          { url: "http://example.com" },
+        ],
+      }
+    end
+
+    let(:invalid_params) do
+      {
+        title: "",
+        error_message: "",
+        content: "",
+        expected_behavior: "",
+        solution: "",
+        cause: "",
+        etc: "",
+        is_solved: false,
+        status: "draft",
+        environments: [],
+        attempts: [],
+        references: [],
+      }
+    end
+
+    context "ログインしている場合" do
+      context "バグが存在する場合" do
+        it "正しい値を入力した場合バグの更新が成功する" do
+          patch "/api/v1/bugs/#{bug.id}", params: valid_params, headers: headers
+
+          expect(response).to have_http_status(:ok)
+          expect(response_json["message"]).to eq("バグを更新しました")
+        end
+
+        it "間違った値を入力した場合バグの更新が失敗し、422 エラーを返す" do
+          patch "/api/v1/bugs/#{bug.id}", params: invalid_params, headers: headers
+
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+
+        it "他人のバグは更新できない" do
+          patch "/api/v1/bugs/#{other_bug.id}", params: valid_params, headers: headers
+
+          expect(response).to have_http_status(:not_found)
+          expect(response_json["message"]).to eq("バグが見つかりません")
+          expect(Bug.find_by(id: other_bug.id)).to eq(other_bug)
+        end
+      end
+
+      context "バグが存在しない場合" do
+        it "バグが見つからないというエラーメッセージを返す" do
+          patch "/api/v1/bugs/#{bug.id + 1}", headers: headers
+
+          expect(response).to have_http_status(:not_found)
+          expect(response_json["message"]).to eq("バグが見つかりません")
+        end
+      end
+    end
+
+    context "ログインしていない場合" do
+      it "401 エラーが返る" do
+        patch "/api/v1/bugs/#{bug.id}"
 
         expect(response).to have_http_status(:unauthorized)
       end
