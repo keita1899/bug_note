c10e53fe0633cdb30959028d43e4341228f24527
keita1899 <saboten18k@gmail.com>
2025-03-19 12:11:04 +0900

    feat: フォロー・フォロー解除のAPI作成

---
+++ backend/app/controllers/api/v1/follows_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::FollowsController < Api::V1::BaseController
+  before_action :authenticate_user!
+  before_action :prevent_duplicate_follow, only: [:create]
+
+  def create
+    user = find_user
+    if user == current_user
+      return render json: { message: "自分をフォローすることはできません" }, status: :unprocessable_entity
+    end
+
+    current_user.follow(user)
+    render json: { message: "フォローしました" }, status: :ok
+  end
+
+  def destroy
+    user = find_user
+    current_user.unfollow(user)
+    render json: { message: "フォローを解除しました" }, status: :ok
+  end
+
+  private
+
+    def find_user
+      User.find(params[:id])
+    end
+
+    def prevent_duplicate_follow
+      user = find_user
+      if current_user.following?(user)
+        render json: { message: "既にフォローしています" }, status: :unprocessable_entity
+      end
+    end
+end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -9,6 +9,10 @@ class User < ApplicationRecord
 
   has_many :bugs, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :active_follows, class_name: "Follow", foreign_key: "follower_id", dependent: :destroy, inverse_of: :follower
+  has_many :following, through: :active_follows, source: :followed
+  has_many :passive_follows, class_name: "Follow", foreign_key: "followed_id", dependent: :destroy, inverse_of: :following
+  has_many :followers, through: :passive_follows, source: :follower
 
   PASSWORD_COMPLEXITY_REGEX = /\A(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,128}\z/
 
@@ -16,4 +20,16 @@ class User < ApplicationRecord
   validates :password,
             format: { with: PASSWORD_COMPLEXITY_REGEX, message: I18n.t("errors.messages.password_complexity") }, if: :new_record?
   validates :password_confirmation, presence: true, if: :new_record?
+
+  def follow(user)
+    active_follows.create(followed_id: user.id)
+  end
+
+  def unfollow(user)
+    active_follows.find_by(followed_id: user.id)&.destroy
+  end
+
+  def following?(user)
+    following.include?(user)
+  end
 end
