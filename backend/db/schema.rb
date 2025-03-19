f8a24e51cda7e59327b02a9422ebe9bec33a0bc2
keita1899 <saboten18k@gmail.com>
2025-03-19 12:06:50 +0900

    chore: followsテーブルを作成するマイグレーション実行

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_14_094427) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_17_081422) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,16 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_14_094427) do
     t.index ["bug_id"], name: "index_environments_on_bug_id"
   end
 
+  create_table "follows", force: :cascade do |t|
+    t.bigint "follower_id", null: false
+    t.bigint "followed_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_follows_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_follows_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
+  end
+
   create_table "references", force: :cascade do |t|
     t.bigint "bug_id", null: false
     t.string "url", null: false
@@ -103,5 +113,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_14_094427) do
   add_foreign_key "comments", "bugs"
   add_foreign_key "comments", "users"
   add_foreign_key "environments", "bugs"
+  add_foreign_key "follows", "users", column: "followed_id"
+  add_foreign_key "follows", "users", column: "follower_id"
   add_foreign_key "references", "bugs"
 end
