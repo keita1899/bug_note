a06a2534c29141ed77fda0c3e09d4954abae05cb
keita1899 <saboten18k@gmail.com>
2025-03-20 21:39:26 +0900

    chore: いいねのカウントのカウンターキャッシュを作成するマイグレーション作成

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_17_081422) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_19_125118) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_17_081422) do
     t.boolean "is_solved", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "likes_count", default: 0, null: false
     t.index ["user_id"], name: "index_bugs_on_user_id"
   end
 
