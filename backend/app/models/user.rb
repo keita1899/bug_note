1f9e59efeed26887e24552d412ceb0528d03f51a
keita1899 <saboten18k@gmail.com>
2025-03-21 22:43:54 +0900

    fix: エラーのためオプションを修正

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :active_follows, class_name: "Follow", foreign_key: "follower_id", dependent: :destroy, inverse_of: :follower
   has_many :following, through: :active_follows, source: :followed
-  has_many :passive_follows, class_name: "Follow", foreign_key: "followed_id", dependent: :destroy, inverse_of: :following
+  has_many :passive_follows, class_name: "Follow", foreign_key: "followed_id", dependent: :destroy, inverse_of: :followed
   has_many :followers, through: :passive_follows, source: :follower
   has_many :likes, dependent: :destroy
   has_many :liked_bugs, through: :likes, source: :bug
