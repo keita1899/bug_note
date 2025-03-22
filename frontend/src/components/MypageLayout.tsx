39186e7d55796f47f210b58761c8731f90a682f4
keita1899 <saboten18k@gmail.com>
2025-03-22 15:30:26 +0900

    npm run format

--- frontend/src/components/MypageLayout.tsx
+++ frontend/src/components/MypageLayout.tsx
@@ -27,7 +27,11 @@ export const MypageLayout = ({ children }: { children: ReactNode }) => {
             <li className={isActive('/mypage/email') ? 'text-primary' : ''}>
               <Link href="">メールアドレス変更</Link>
             </li>
-            <li className={isActive('/mypage/account-delete') ? 'text-primary' : ''}>
+            <li
+              className={
+                isActive('/mypage/account-delete') ? 'text-primary' : ''
+              }
+            >
               <Link href="/mypage/account-delete">アカウント削除</Link>
             </li>
           </ul>
