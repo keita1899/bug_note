33de59867b132e289db334060055cc4366f8619e
keita1899 <saboten18k@gmail.com>
2025-03-24 20:58:01 +0900

    refactor: Tabの型をジェネリック型を使って汎用的に修正

--- frontend/src/features/mypage/ui/Tabs.tsx
+++ frontend/src/features/mypage/ui/Tabs.tsx
@@ -1,21 +1,26 @@
 import { Tab } from '../types/Tab'
-import { TabValue } from '../types/TabValue'
 
-type TabsProps = {
-  tabs: Tab[]
-  selectedTab: TabValue
-  onClick: (tabValue: TabValue) => void
+type TabsProps<T extends string> = {
+  tabs: Tab<T>[]
+  selectedTab: T
+  onClick: (tabValue: T) => void
 }
 
-export const Tabs = ({ tabs, selectedTab, onClick }: TabsProps) => {
+export const Tabs = <T extends string>({
+  tabs,
+  selectedTab,
+  onClick,
+}: TabsProps<T>) => {
   return (
     <div>
       <div role="tablist" className="tabs">
         {tabs.map((tab) => (
           <a
-            key={tab.value}
+            key={String(tab.value)}
             role="tab"
-            className={`tab font-bold ${selectedTab === tab.value ? 'tab-active text-primary' : ''}`}
+            className={`tab font-bold ${
+              selectedTab === tab.value ? 'tab-active text-primary' : ''
+            }`}
             onClick={() => onClick(tab.value)}
           >
             {tab.label}
