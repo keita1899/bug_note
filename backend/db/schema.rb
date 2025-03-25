11374f38a9bad38e1ab6faffe672fd79ea244679
keita1899 <saboten18k@gmail.com>
2025-03-25 19:01:35 +0900

    chore: usersテーブルにプロフィールの情報のカラムを追加するマイグレーショん実行

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_19_125118) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_25_095626) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -113,6 +113,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_19_125118) do
     t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "bio"
+    t.string "github_url", limit: 255
+    t.string "website_url", limit: 255
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
